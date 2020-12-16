562ac3fa1ec45e5b7ebec406ac67ceddbd079711
Lyaa-A <178341670@qq.com>
2020-12-17 01:34:23 +0800

    第一次提交helloworld

---
+++ hello_world.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-08 16:20:34
+ * @LastEditTime: 2020-12-11 00:33:59
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \C_primer_plus\11.c
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world\n");
+    return 0;
+}
