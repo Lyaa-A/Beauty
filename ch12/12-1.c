0b0b0fd4be114b5f761609cd6308b265bb0aae4f
Lyaa-A <178341670@qq.com>
2020-12-17 20:35:39 +0800

    trytry

---
+++ ch12/12-1.c
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-17 20:35:16
+ * @LastEditTime: 2020-12-17 20:35:16
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \C_primer_plus\ch12\12-1.c
+ */
